8326d587808c5504a137a390ab1b2861e195d5ce
Mutoi01 <pngando9@gmail.com>
2022-07-18 13:06:44 +0300

    commit tree 16d48420734440dec1c92e2a2db1be64acda2c94

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe (){
+  function handleFocus(event){
+    return console.log('Good!');
+  }
+  function handleBlur(event){
+    return console.log('Hey! Eyes on me!');
+  }
+  return(
+    <>
+    <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    </>
+  )
+}
+
+export default EyesOnMe
